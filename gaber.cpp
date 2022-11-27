8df836485a63ed89117a86b700f0eae806186a42
gaber <mohamedthebraveheart@gmail.com>
2022-11-27 22:57:10 +0200

    creating hello message

---
+++ gaber.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    cout << "gaber\n";
+}
---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class hello {
+     public static void main(String[] args) {
+         
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       System.out.println(n);
+
+    }
+}
